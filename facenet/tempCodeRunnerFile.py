654a5340cb301a6f742eab814e470f1a3f8d193b
Sujit-Hiwale <sujithiwale88@gmail.com>
2025-10-13 15:04:32 +0530

    Database changes

--- facenet/tempCodeRunnerFile.py
+++ facenet/tempCodeRunnerFile.py
@@ -5,6 +5,7 @@ from keras_facenet import FaceNet
 
 # Initialize Flask
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 CORS(app)
 
 # Initialize DB
@@ -20,4 +21,8 @@ from routes import register_routes
 register_routes(app, db, Attendance, detector, embedder)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(
+    host="0.0.0.0", 
+    port=5000, 
+    ssl_context=("../10.68.222.139+2.pem", "../10.68.222.139+2-key.pem")
+)
